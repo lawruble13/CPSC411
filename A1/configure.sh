a99040baaf82117e5ac533de8f2b85fa583c7c07
Liam Wrubleski <liam@wrubleski.net>
2019-02-04 06:01:09 -0700

    Symbolically link man

--- A1/configure.sh
+++ A1/configure.sh
@@ -4,7 +4,7 @@ if [ ! -w . ]; then
     exit 1
 fi
 
-echo """lexer:
+echo """lexer: scanner_man
 	flex a1.flex
 	gcc -o scanner lex.yy.c -lfl
 
@@ -31,8 +31,34 @@ echo -e """clean_lexer:
 clean_test:
 \trm -f *.res
 
-clean: clean_lexer clean_test
+clean_man:
+\tif [ -L /usr/local/man ] && [ \"\"\$\$(readlink /usr/local/man)\"\" = \"\"\$\$(pwd)/man\"\" ]; then \
+\tunlink /usr/local/man; \
+\telif [ -L /usr/local/man/man1 ] && [ \"\"\$\$(readlink /usr/local/man/man1)\"\" = \"\"\$\$(pwd)/man/man1\"\" ]; then \
+\tunlink /usr/local/man/man1; \
+\telif [ -L /usr/local/man/man1/scanner.1 ] && [ \"\"\$\$(readlink /usr/local/man/man1/scanner.1)\"\" = \"\"\$\$(pwd)/man/man1/scanner.1\"\" ]; then \
+\tunlink /usr/local/man/man1/scanner.1; \
+\tfi
+\techo unlinking
+\tmandb >/dev/null 2>&1
+
+clean: clean_lexer clean_test clean_man
 
 deep_clean: clean
 \trm Makefile
+
+scanner_man:
+\tif [ -d /usr/local/man ]; then \
+\tif [ -d /usr/local/man/man1 ]; then \
+\tln -s `pwd`/man/man1/scanner.1 /usr/local/man/man1/scanner.1; \
+\techo \"attempting to link to man/man1/scanner.1\"; \
+\telse \
+\tln -s `pwd`/man/man1 /usr/local/man/man1; \
+\techo \"attempting to link to man/man1\"; \
+\tfi; \
+\telse \
+\tln -s `pwd`/man /usr/local/man; \
+\techo \"attempting to link to man\"; \
+\tfi;
+\tmandb
 """ >> Makefile
